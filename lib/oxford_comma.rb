49d442da4cca24be8f66f033c657c6e2b8f34949
root <gloria.kitili@student.moringaschool.com>
2023-05-20 13:49:27 +0300

    join,arrays

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+ case array.length
+ when 1 
+    array[0]
+ when 2
+    array.join(' and ')
+ else
+    last_element = array.pop
+    "#{array.join(', ')}, and #{last_element}"
 end
+end
+puts oxford_comma(['kiwi'])
+puts oxford_comma(['kiwi','durian'])
+
+puts oxford_comma(['kiwi','durian','starfruit'])
+puts oxford_comma(['kiwi','durian','starfruit','mangos'])
+puts oxford_comma(['kiwi','durian','starfruit','mangos','dragon fruits'])
